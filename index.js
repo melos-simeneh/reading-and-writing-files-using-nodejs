e8b9cfa50e15748d739d2b2c6d4ccf432643e145
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-12 09:49:07 -0700

    writting to file

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ console.log("Reading and Writing Files");
 
 const textIn = fs.readFileSync("./text/input.txt", "utf-8");
 console.log(textIn);
+
+const textOut = "This is text written to output.txt file";
+fs.writeFileSync("./text/output.txt", textOut);
+console.log("text written to file");
