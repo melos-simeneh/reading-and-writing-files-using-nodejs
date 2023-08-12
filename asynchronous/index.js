ade9f767838b1eb8b937715cde5d02212b2a838c
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-12 10:29:13 -0700

    sync and async update

--- asynchronous/index.js
+++ asynchronous/index.js
@@ -1,12 +1,19 @@
 const fs = require("fs");
 
-console.log("Reading and Writing Files");
+console.log("Reading and Writing Files Asynchronously");
 
-const textIn = fs.readFile("./text/input.txt", "utf-8");
-console.log(textIn);
+//Non-blocking reading and wrting, asynchronous way
+
+fs.readFile("./txt/input.txt", "utf-8", (err, text) => {
+  if (err) return console.log("Error while reading file");
+  console.log(text);
+});
+console.log("After reading file");
 
 const textOut = "This is text written to output.txt file";
-fs.writeFile("./text/output.txt", textOut, (err, data) => {
-  console.log(`${data} written to file`);
+fs.writeFile("./txt/output.txt", textOut, (err) => {
+  if (err) return console.log("Error while writting to file");
+  console.log("text written to file");
 });
-console.log("text written to file");
+
+console.log("After writting to file");
--- synchronous/index.js
+++ synchronous/index.js
@@ -1,10 +1,14 @@
 const fs = require("fs");
-
-console.log("Reading and Writing Files");
+//Blocking, synchronous way
+console.log("Reading and Writing Files Synchronously");
 
 const textIn = fs.readFileSync("./text/input.txt", "utf-8");
 console.log(textIn);
 
+console.log("After reading file");
+
 const textOut = "This is text written to output.txt file";
 fs.writeFileSync("./text/output.txt", textOut);
 console.log("text written to file");
+
+console.log("After writting to file");
