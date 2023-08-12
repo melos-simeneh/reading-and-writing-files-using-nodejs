b3650379c543ee6cf95350ef6073ed73ec75f974
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-12 10:46:04 -0700

    output images

--- synchronous/index.js
+++ synchronous/index.js
@@ -2,13 +2,13 @@ const fs = require("fs");
 //Blocking, synchronous way
 console.log("Reading and Writing Files Synchronously");
 
-const textIn = fs.readFileSync("./text/input.txt", "utf-8");
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
 
 console.log("After reading file");
 
 const textOut = "This is text written to output.txt file";
-fs.writeFileSync("./text/output.txt", textOut);
+fs.writeFileSync("./txt/output.txt", textOut);
 console.log("text written to file");
 
 console.log("After writting to file");
